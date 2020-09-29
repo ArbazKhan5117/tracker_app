aa0e1d451e3e8634f2c0c17aece8fdc8a5bcda70
ArbazKhan5117 <arbazkhan5117@gmail.com>
2020-09-29 09:54:11 +0000

    Update transReducer.js

--- src/transReducer.js
+++ src/transReducer.js
@@ -1,13 +1,14 @@
 const transactionReducer=((state,action)=>{
     switch(action.type){
         case 'Add_Transaction':{
-            return[action.payload,...state];
+            return[action.payload,...state]
         }
         case 'Del_Transaction':{
             if(action.count===1){
-                action.count=2;
-                state.splice(action.key,1);
+                action.count=2
+                state.splice(action.key,1)
             }
+            break;
         }
         default:{
             return(state);
